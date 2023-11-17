816539c0010a94d58720eb3291ae446d516f0e34
Prateek <prateek.kumar.original@gmail.com>
2023-11-18 02:35:02 +0530

    Implement pagination

--- forsale_project/shop/views.py
+++ forsale_project/shop/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
     product_objects = Product.objects.all()
+
+    # search code
     item_name = request.GET.get('item_name')
     if item_name is not None and item_name != "":
         product_objects = product_objects.filter(title__icontains=item_name)
+    
+    # pagination
+    paginator = Paginator(product_objects, 2)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
     return render(request, 'shop/index.html', {"product_objects":product_objects})
